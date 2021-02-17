f361216cf1a2f5a2001f9af1e468b38d4428881f
kashyapbari <bari.r.kash@gmail.com>
2021-02-18 02:10:49 +0530

    improvized universe testcases

--- src/test/java/com/kashyapbari/gameoflife/UniverseTest.java
+++ src/test/java/com/kashyapbari/gameoflife/UniverseTest.java
@@ -1,66 +1,194 @@
 package com.kashyapbari.gameoflife;
 
 import com.kashyapbari.gameoflife.model.Cell;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
 
-class UniverseTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public static String[] cellSparseMatrix(){
-        return new String[] {
-                "1, 1 \n 1, 2 \n 2, 1 \n 2, 2",
-                "00, 1 \n1, 0 \n   2, 1 \n0,    2\n 1,2"
-        };
-    }
+public class UniverseTest {
 
-    @ParameterizedTest
-    @MethodSource(value = "cellSparseMatrix")
-    public void testCellSetFromString(String cellMatrix){
-        Universe universe = new Universe(cellMatrix);
-        assertNotNull(universe);
-    }
+    @Test
+    @DisplayName("One cell In an Empty Universe Dies")
+    public void oneCellInAnEmptyUniverseDies() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(0, 0));
+        Universe universe = new Universe(aliveSet);
 
-    public static String[] cellPlay(){
-        return new String[] {
-//                cell to play, active cells in universe, playing cell current state, playing cell new state
-                "1,1:1, 1 \n 1, 2 \n 2, 1 \n 2, 2:true:true",
-                "1,0:1,1 \n1,0 \n 1,2:true:false",
-                "1,1:1,1 \n1,0 \n 1,2:true:true",
-                "1,4:1, 1\n1, 2\n1, 3\n2, 2\n2, 3\n2, 4:false:true",
-                "3,3:1, 1\n1, 2\n1, 3\n2, 2\n2, 3\n2, 4:false:true"
-        };
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "cellPlay")
-    public void testplayCell(String cellPlays){
-        String[] cellPlay = cellPlays.split(":");
-        Universe universe = new Universe(cellPlay[1].strip());
-        Cell cell = new Cell(cellPlay[0].strip());
-        Boolean aliveFlag = Boolean.parseBoolean(cellPlay[2].strip());
-        Boolean result = Boolean.parseBoolean(cellPlay[3].strip());
-        assertEquals(result, universe.playCell(cell,aliveFlag));
-    }
-
-
-    public static String[] tickPlayState(){
-        return new String[] {
-            "0, 1\n1, 0\n2, 1\n0, 2\n1, 2 : 0, 1\n1, 0\n2, 1\n0, 2\n1, 2 ",
-            "1, 1\n1, 0\n1, 2 : 1, 1\n0, 1\n2, 1",
-            "1, 1\n1, 2\n1, 3\n2, 2\n2, 3\n2, 4 : 0, 2\n1, 1\n1, 4\n2, 1\n2, 4\n3, 3"
-        };
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "tickPlayState")
-    void testplayTick(String tickPlays) {
-        String[] tickPlay = tickPlays.split(":");
-        Universe universe = new Universe(tickPlay[0]);
         universe.playTick(1);
-        Universe nextUniverse = new Universe(tickPlay[1]);
-        assertEquals(universe, nextUniverse);
+
+        assertEquals(universe, new Universe(new HashSet<>()));
     }
+
+    @Test
+    @DisplayName(("Two Neighbouring cells Die"))
+    public void twoNeighbouringCellsDie() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(0, 0));
+        aliveSet.add(new Cell(0, 1));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assertEquals(universe, new Universe(new HashSet<>()));
+    }
+
+    @Test
+    @DisplayName("Three Far away cells Die")
+    public void threeSeperateCellsDie() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(0, 0));
+        aliveSet.add(new Cell(2, 0));
+        aliveSet.add(new Cell(2, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assertEquals(universe, new Universe(new HashSet<>()));
+    }
+
+    @Test
+    @DisplayName("Cell with Two neighbours Lives")
+    public void cellWithTwoNeighboursLives() {
+        Cell cell = new Cell(1, 1);
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(cell);
+        aliveSet.add(new Cell(0, 0));
+        aliveSet.add(new Cell(2, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assert (universe.getAlive().contains(cell));
+    }
+
+    @Test
+    @DisplayName("Cell with Three neighbours Lives")
+    public void cellWithThreeNeighboursLives() {
+        Cell cell = new Cell(1, 1);
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(cell);
+        aliveSet.add(new Cell(0, 0));
+        aliveSet.add(new Cell(2, 2));
+        aliveSet.add(new Cell(0, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assert (universe.getAlive().contains(cell));
+
+    }
+
+
+    @Test
+    @DisplayName("Cell with more than Three neighbours Dies")
+    public void cellWithMoreThanThreeNeighboursDies() {
+        Cell cell = new Cell(1, 1);
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(cell);
+        aliveSet.add(new Cell(0, 0));
+        aliveSet.add(new Cell(2, 2));
+        aliveSet.add(new Cell(0, 2));
+        aliveSet.add(new Cell(2, 0));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assert (!universe.getAlive().contains(cell));
+    }
+
+    @Test
+    @DisplayName("Cell with Exactly Three Neighbours Resurrects")
+    public void cellWithExactlyThreeNeighboursResurrects() {
+        Cell cell = new Cell(1, 1);
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(0, 0));
+        aliveSet.add(new Cell(2, 2));
+        aliveSet.add(new Cell(0, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assert (universe.getAlive().contains(cell));
+    }
+
+    @Test
+    @DisplayName("Block Pattern - Still Life")
+    public void blockPattern() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(1, 1));
+        aliveSet.add(new Cell(1, 2));
+        aliveSet.add(new Cell(2, 1));
+        aliveSet.add(new Cell(2, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assertEquals(universe, new Universe(new HashSet<>(aliveSet)));
+    }
+
+    @Test
+    @DisplayName("Boat Pattern - Still Life")
+    public void BoatPattern() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(0, 1));
+        aliveSet.add(new Cell(1, 0));
+        aliveSet.add(new Cell(2, 1));
+        aliveSet.add(new Cell(0, 2));
+        aliveSet.add(new Cell(1, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        assertEquals(universe, new Universe(new HashSet<>(aliveSet)));
+    }
+
+    @Test
+    @DisplayName("Blinker Pattern - Oscillator")
+    public void BlinkerPattern() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(1, 1));
+        aliveSet.add(new Cell(1, 0));
+        aliveSet.add(new Cell(1, 2));
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        HashSet<Cell> newAliveSet = new HashSet<>();
+        newAliveSet.add(new Cell(1, 1));
+        newAliveSet.add(new Cell(0, 1));
+        newAliveSet.add(new Cell(2, 1));
+
+        assertEquals(universe, new Universe(newAliveSet));
+    }
+
+    @Test
+    @DisplayName("Toad pattern - two phase oscillator")
+    public void ToadPattern() {
+        HashSet<Cell> aliveSet = new HashSet<>();
+        aliveSet.add(new Cell(1, 1));
+        aliveSet.add(new Cell(1, 2));
+        aliveSet.add(new Cell(1, 3));
+        aliveSet.add(new Cell(2, 2));
+        aliveSet.add(new Cell(2, 3));
+        aliveSet.add(new Cell(2, 4));
+
+        Universe universe = new Universe(aliveSet);
+
+        universe.playTick(1);
+
+        HashSet<Cell> newAliveSet = new HashSet<>();
+        newAliveSet.add(new Cell(0, 2));
+        newAliveSet.add(new Cell(1, 1));
+        newAliveSet.add(new Cell(1, 4));
+        newAliveSet.add(new Cell(2, 1));
+        newAliveSet.add(new Cell(2, 4));
+        newAliveSet.add(new Cell(3, 3));
+
+        assertEquals(universe, new Universe(newAliveSet));
+    }
+
+
 }
